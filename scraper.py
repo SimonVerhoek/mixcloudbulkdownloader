2960d8cb8abece536125d56b0ed4aa4bc986fb22
Simon Verhoek <simon@simonic.nl>
2020-02-19 15:25:14 +0100

    script works

--- scraper.py
+++ scraper.py
@@ -1,27 +1,37 @@
-import requests, json, sys
- 
+import json, sys
+
+import requests
+
+
 if len(sys.argv) != 2:
-    print("Please specify a mixcloud account name to grab all Cloudcast links: mget.py keepingtheravealive")
+    print("Please specify a mixcloud account name to grab all Cloudcast links: "
+          "mget.py keepingtheravealive")
     sys.exit()
-   
-r = requests.get("https://api.mixcloud.com/" + sys.argv[1] + "/cloudcasts/")
+
+artist_name = sys.argv[1]
+
+r = requests.get(f'https://api.mixcloud.com/{artist_name}/cloudcasts/')
 json_r = json.loads(r.text)
 full_list = []
- 
+
 if 'data' not in json_r:
     print("The username was not found or the user does not have any 'shows' on their profile")
     sys.exit()
- 
+
+data = json_r['data']
+
 while True:
-    for i in range(0, len(json_r["data"])):
-        full_list.append("https://mixcloud.com"+json_r["data"][i]["key"])
-    if 'next' not in json_r["paging"]: break
-    r = requests.get(json_r["paging"]["next"])
+    for i in range(0, len(data)):
+        full_list.append(f"https://mixcloud.com{data[i]['key']}")
+    if 'next' not in json_r['paging']:
+        break
+    r = requests.get(json_r['paging']['next'])
     json_r = json.loads(r.text)
-   
-with open('mc_list_'+sys.argv[1], 'w') as f:
+
+with open(f'mc_list_{artist_name}', 'w') as f:
     for item in full_list:
-        f.write("%s\n" % item)
-       
-print("Found "+str(len(full_list)) + " links for user \""+sys.argv[1]+"\". All links are in the file mc_list_"+sys.argv[1])
-print("Use this list with youtube-dl and the parameter -a mc_list_"+sys.argv[1])
+        f.write(f'{item}\n')
+
+print(f'Found {str(len(full_list))} links for user "{artist_name}". '
+      f'All links are in the file mc_list_{artist_name}')
+print(f"Use this list with youtube-dl and the parameter -a mc_list_{artist_name}")
