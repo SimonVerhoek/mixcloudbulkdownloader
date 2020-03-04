af1f7cf61555c62080281de9921436e224ef5da9
Simon Verhoek <simon@simonic.nl>
2020-03-04 19:52:01 +0100

    added select_all and unselect_all buttons

--- mcdlr/main.py
+++ mcdlr/main.py
@@ -12,6 +12,7 @@ from PySide2.QtWidgets import (
     QMainWindow,
     QVBoxLayout,
     QWidget,
+    QPushButton
 )
 
 from .api import search_user_API_url, user_cloudcasts_API_url
@@ -25,41 +26,47 @@ class Widget(QWidget):
 
         self.layout = QVBoxLayout()
 
-        self.search_user_layout = QHBoxLayout()
-        self.search_user_layout.setAlignment(Qt.AlignTop)
+        search_user_layout = QHBoxLayout()
+        search_user_layout.setAlignment(Qt.AlignTop)
 
         self.search_user_label = QLabel('Search account/artist:')
-        self.search_user_layout.addWidget(self.search_user_label)
+        search_user_layout.addWidget(self.search_user_label)
 
         self.search_user_input = QLineEdit()
-        self.search_user_layout.addWidget(self.search_user_input)
+        search_user_layout.addWidget(self.search_user_input)
 
-        self.search_user_result_layout = QVBoxLayout()
+        search_user_result_layout = QVBoxLayout()
         self.search_user_results_list = QListWidget()
-        self.search_user_result_layout.addWidget(self.search_user_results_list)
+        search_user_result_layout.addWidget(self.search_user_results_list)
 
-        self.user_cloudcasts_layout = QVBoxLayout()
+        user_cloudcasts_layout = QVBoxLayout()
         self.user_cloudcasts_results = QListWidget()
-        self.user_cloudcasts_layout.addWidget(self.user_cloudcasts_results)
+        user_cloudcasts_layout.addWidget(self.user_cloudcasts_results)
 
-        self.layout.addLayout(self.search_user_layout)
-        self.layout.addLayout(self.search_user_result_layout)
-        self.layout.addLayout(self.user_cloudcasts_layout)
+        cloudcast_action_buttons = QHBoxLayout()
+        self.select_all_button = QPushButton('Select All')
+        self.unselect_all_button = QPushButton('Unselect All')
+        self.cancel_button = QPushButton('Cancel')
+        self.download_button = QPushButton('Download')
+        cloudcast_action_buttons.addWidget(self.select_all_button)
+        cloudcast_action_buttons.addWidget(self.unselect_all_button)
+        cloudcast_action_buttons.addWidget(self.cancel_button)
+        cloudcast_action_buttons.addWidget(self.download_button)
+
+        self.layout.addLayout(search_user_layout)
+        self.layout.addLayout(search_user_result_layout)
+        self.layout.addLayout(user_cloudcasts_layout)
+        self.layout.addLayout(cloudcast_action_buttons)
 
         self.setLayout(self.layout)
 
+        # connections
         self._connect_with_delay(
             input=self.search_user_input.textChanged[str],
             slot=self.search_account
         )
-
-    def _connect_with_delay(self, input: Callable, slot: Slot, delay_ms: int = 750):
-        """Connects a given input to a given Slot with a given delay."""
-        self.timer = QTimer()
-        self.timer.setInterval(delay_ms)
-        self.timer.setSingleShot(True)
-        self.timer.timeout.connect(slot)
-        input.connect(self.timer.start)
+        self.select_all_button.clicked.connect(self.select_all)
+        self.unselect_all_button.clicked.connect(self.unselect_all)
 
     @Slot()
     def search_account(self):
@@ -84,6 +91,24 @@ class Widget(QWidget):
         username = self.search_user_results_list.currentItem().user.username
         self._query_cloudcasts(username=username)
 
+    @Slot()
+    def select_all(self):
+        for i in range(self.user_cloudcasts_results.count()):
+            self.user_cloudcasts_results.item(i).setCheckState(Qt.Checked)
+
+    @Slot()
+    def unselect_all(self):
+        for i in range(self.user_cloudcasts_results.count()):
+            self.user_cloudcasts_results.item(i).setCheckState(Qt.Unchecked)
+
+    def _connect_with_delay(self, input: Callable, slot: Slot, delay_ms: int = 750):
+        """Connects a given input to a given Slot with a given delay."""
+        self.timer = QTimer()
+        self.timer.setInterval(delay_ms)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(slot)
+        input.connect(self.timer.start)
+
     def _query_cloudcasts(self, username: str, url: str = ''):
         if not url:
             url = user_cloudcasts_API_url(username=username)
@@ -106,6 +131,13 @@ class Widget(QWidget):
         if response.get('paging') and response['paging'].get('next'):
             next_url = response['paging'].get('next')
             self._query_cloudcasts(username=username, url=next_url)
+
+    def _get_all_checked_cloudcasts(self):
+        selected_cloudcasts = []
+        for i in range(self.user_cloudcasts_results.count()):
+            if self.user_cloudcasts_results.item(i).checkState() == Qt.Checked:
+                selected_cloudcasts.append(self.user_cloudcasts_results.item(i))
+        return selected_cloudcasts
 
 
 class MainWindow(QMainWindow):
