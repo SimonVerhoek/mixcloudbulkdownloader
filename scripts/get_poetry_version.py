46bf1b9dc766708b2a8da1bd3982028a7442b865
Simon Verhoek <simon@simonic.nl>
2026-08-19 14:01:08 +0200

    fix breaking macOS build

--- scripts/get_poetry_version.py
+++ scripts/get_poetry_version.py
@@ -7,10 +7,10 @@ ROOT_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def get_poetry_version(root_dir: Path = ROOT_DIR) -> str:
-    """Reads version from poetry config file."""
+    """Reads version from pyproject.toml [project] table."""
     with open(root_dir / "pyproject.toml", "rb") as f:
         data = tomllib.load(f)
-        return data["tool"]["poetry"]["version"]
+        return data["project"]["version"]
 
 
 if __name__ == "__main__":
